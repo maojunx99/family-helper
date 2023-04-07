a74184f6073a40ad2e3b772a16daf10fcb4ecdaa
maojunx99 <maojunx99@gmail.com>
2023-04-07 15:30:46 -0500

    auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Amplify } from 'aws-amplify';
+import awsConfig from './aws-exports';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(awsConfig);
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Maojun's Website</h1>
-    </div>
+    <Authenticator>
+      {({ signout, user }) => (
+        <div className="App">
+          <h1>Welcome to Maojun's Website</h1>
+          <button onClick={signout}>Sign out</button>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
